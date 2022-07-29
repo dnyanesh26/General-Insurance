a0aa31128ab2acc5ee10c5044bd68d00c495aa34
Cloudstrif9 <rohitraj1934@gmail.com>
2022-07-29 10:49:27 +0530

    FAQ

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FAQComponent } from './Components/faq/faq.component';
 import { HomepageComponent } from './Components/homepage/homepage.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
@@ -7,7 +8,8 @@ import { RegisterComponent } from './Components/register/register.component';
 const routes: Routes = [
   { path:'',component:HomepageComponent},
   { path:'login',component:LoginComponent},
-  { path:'register',component:RegisterComponent}
+  { path:'register',component:RegisterComponent},
+  {path:'faq',component:FAQComponent}
   
 
 ];
